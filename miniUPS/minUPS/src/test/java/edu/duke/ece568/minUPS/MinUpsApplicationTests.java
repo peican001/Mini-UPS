102dc6653e228a2c884f80701b3076cae808c6f1
Can Pei <125683162+peican001@users.noreply.github.com>
2023-06-03 18:59:37 +0800

    Add files via upload

---
+++ miniUPS/minUPS/src/test/java/edu/duke/ece568/minUPS/MinUpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.duke.ece568.minUPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinUpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
